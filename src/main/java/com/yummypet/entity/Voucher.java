0b17fb1a5a4d08cd7c6231633a7083502088f460
Phuonghv248 <phuonghv248@gmail.com>
2025-06-27 22:14:39 +0700

    Entity Voucher

--- src/main/java/com/yummypet/entity/Voucher.java
+++ src/main/java/com/yummypet/entity/Voucher.java
@@ -1,5 +1,59 @@
 package com.yummypet.entity;
 
-public class Voucher {
 
+import com.yummypet.enums.DiscountType;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "vouchers")
+public class Voucher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false,unique = true,length = 50)
+    private  String code;
+
+    @Column(nullable = false,length = 200)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "discount_type", nullable = false)
+    private DiscountType discountType;
+
+    @Column(name = "discount_value",nullable = false)
+    private BigDecimal discountValue;
+
+    @Column(name = "min_order_amount")
+    private BigDecimal minOrderAmount;
+
+    @Column(name = "max_discount_amount")
+    private BigDecimal maxDiscountAmount;
+
+    @Column(name = "usage_limit")
+    private Integer usageLimit;
+
+    @Column(name = "used_count")
+    private Integer usedCount = 0;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+
+    @Column(name = "is_active")
+    private Boolean isActive = true;
+
+    @Column(name = "created_at")
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
 }
